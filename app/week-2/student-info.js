b41322e5fca28f8563e104be0d64d52133e96b6f
YukiHyuga <daveluna19@yahoo.com>
2024-01-19 12:04:01 -0700

    week 2 assignment
    This is a submission for week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+
+const StudentInfo = () => {
+    return (
+        <div>
+            <p>Dave Luna</p>
+            <Link href={"https://github.com/YukiHyuga/cprg306-assignments"}>Link to GitHub Repository</Link>
+        </div>
+    );
+};
+export default StudentInfo;
