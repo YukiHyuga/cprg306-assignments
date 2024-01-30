1ae8b75d4ec9e3181194d8d85b9d66f2a6d05971
YukiHyuga <daveluna19@yahoo.com>
2024-01-29 19:49:01 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,10 +4,10 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm">
-      <h1 className="text-4xl font-bold mb-5 border-4 border-pink-400 p-10">CPRG 306: Web Development 2 - Assignments</h1>
-      <div className="text-lg">
-      <li className="hover:text-pink-400 hover:border-hover">
+      <div className="items-center justify-between text-sm">
+      <h1 className="text-5xl font-bold mb-4 border-4 border-black p-10">CPRG 306: Web Development 2 - Assignments</h1>
+      <div className="text-3xl border-4 border-black p-10">
+      <li className="hover:text-pink-400">
       <Link href={"/week-2"}>Week 2</Link>
       </li>
       </div>
